ce0900e7a24ec905b10eb9c984628060ef5432b5
Kailuo Wang <Kailuo.wang@gmail.com>
2011-03-02 01:38:47 -0500

    auto use medium size when too high

--- public/javascripts/slideshow.js
+++ public/javascripts/slideshow.js
@@ -9,7 +9,7 @@ function preload(arrayOfImages) {
 
 $j(document).ready(function() {
     pic = $j('#picture')[0];
-    if(pic.width == 500 && pic.height == 375){
+    if((pic.width == 500 && pic.height == 375) || pic.height > $j(window).height()){
        pic.src = medium_url;
     }
 
