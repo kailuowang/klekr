516f26e4acf629dc4f0f02f4d7737e9c778d3ee3
Kailuo Wang <Kailuo.wang@gmail.com>
2011-03-14 22:11:58 -0400

    minor fixes

--- spec/factories/picture_factory.rb
+++ spec/factories/picture_factory.rb
@@ -1,3 +1,18 @@
+Factory.sequence :pic_info do |n|
+  FlickRaw::Response.new({"id"    =>  "#{n}",
+                          "owner" =>  '23242325@N00',
+                          "secret"=>  "dfd91ef806",
+                          "server"=>  "5162",
+                          "farm"  =>  6,
+                          "title" =>  "a test picture created by factory",
+                          "ispublic"  =>  1,
+                          "isfriend"  =>  0,
+                          "isfamily"  =>  0,
+                          "ownername"=>  'John Kim',
+                          "dateupload"=>"1294841334",
+                          "date_faved"=>"1298678009"}, "photo")
+end
+
 Factory.define :picture, :class => Picture do |p|
   pic_info = Factory.next(:pic_info)
   p.title pic_info.title
