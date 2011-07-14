2cf39def72a4d5abe92011bb2760f96016ffb2e3
Kailuo Wang <kailuo.wang@gmail.com>
2011-07-13 22:12:59 -0400

    assets pipeline works now

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module Collectr
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.assets.enabled = true
 
   end
 end
