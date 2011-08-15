0e40816d1a3b62c46f0359af2dedf818651ac0e4
Kailuo Wang <Kailuo.wang@gmail.com>
2011-08-15 13:44:11 -0400

    fixed minor bug in rake helper

--- lib/collectr/rake_helper.rb
+++ lib/collectr/rake_helper.rb
@@ -1,7 +1,7 @@
 module Collectr::RakeHelper
 
   def output(msg)
-    if(Rails.env == :production)
+    if(Rails.env == 'production')
       puts(DateTime.now.to_s + ": " + msg)
     else
       Rails.logger.info(msg)
