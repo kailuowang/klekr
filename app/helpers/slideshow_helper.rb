61b882a85c0d5bb20ebd72f5a1d542de0d2faaeb
Kailuo Wang <kailuo.wang@gmail.com>
2012-02-16 09:25:43 -0500

    minor style change

--- app/helpers/slideshow_helper.rb
+++ app/helpers/slideshow_helper.rb
@@ -6,7 +6,7 @@ module SlideshowHelper
   def render_bottom_link(link_name)
     case link_name
       when :editors_choice_rss
-         link_to (image_tag('feed-icon-small.png') + " RSS").html_safe, editors_choice_path(format: :rss)
+         link_to (image_tag('feed-icon-small.png', class: 'rss-icon') + " RSS").html_safe, editors_choice_path(format: :rss)
       when :editors_choice
         link_to "Editor's Choice" , editors_choice_path
       when :editor_choice_google_currents
