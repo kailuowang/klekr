63aab92db0f17d69888fd75ec8d1dba97ba12b91
Kailuo Wang <kailuo.wang@gmail.com>
2012-04-24 23:20:09 -0400

    fixing the pic info error

--- lib/collectr/picture_controller_helper.rb
+++ lib/collectr/picture_controller_helper.rb
@@ -1,31 +1,35 @@
 module Collectr
   module PictureControllerHelper
     def data_for(picture)
-      {
-        id:               picture.string_id,
-        largeUrl:         picture.large_url,
-        mediumUrl:        picture.medium_url,
-        mediumSmallUrl:   picture.medium_small_url,
-        smallUrl:         picture.small_url,
-        interestingness:  picture.stream_rating.to_i,
-        title:            picture.display_title,
-        description:      picture.no_longer_valid? ? 'SORRY. This picture is no longer available.' : picture.description,
-        flickrPageUrl:    picture.url,
-        ownerName:        picture.owner_name,
-        ownerId:          picture.owner_id,
-        noLongerValid:    picture.no_longer_valid?,
-        faved:            picture.faved?,
-        favedDate:        picture.faved_at ? picture.faved_at.to_date.to_s(:long) : nil,
-        rating:           picture.rating,
-        viewed:           picture.viewed?,
-        ofCurrentCollector: picture.collector == current_collector,
-        inKlekr:          !picture.new_record?,
-        dateUpload:       picture.date_upload,
-        ownerPath:        user_path(picture.owner_id),
-        fromStreams:      picture.flickr_streams.map do |stream|
-                            data_for_stream_info(stream)
-                          end
-      }.merge action_paths_for(picture)
+      if (picture.pic_info)
+        {
+          id:               picture.string_id,
+          largeUrl:         picture.large_url,
+          mediumUrl:        picture.medium_url,
+          mediumSmallUrl:   picture.medium_small_url,
+          smallUrl:         picture.small_url,
+          interestingness:  picture.stream_rating.to_i,
+          title:            picture.display_title,
+          description:      picture.no_longer_valid? ? 'SORRY. This picture is no longer available.' : picture.description,
+          flickrPageUrl:    picture.url,
+          ownerName:        picture.owner_name,
+          ownerId:          picture.owner_id,
+          noLongerValid:    picture.no_longer_valid?,
+          faved:            picture.faved?,
+          favedDate:        picture.faved_at ? picture.faved_at.to_date.to_s(:long) : nil,
+          rating:           picture.rating,
+          viewed:           picture.viewed?,
+          ofCurrentCollector: picture.collector == current_collector,
+          inKlekr:          !picture.new_record?,
+          dateUpload:       picture.date_upload,
+          ownerPath:        user_path(picture.owner_id),
+          fromStreams:      picture.flickr_streams.map do |stream|
+                              data_for_stream_info(stream)
+                            end
+        }.merge action_paths_for(picture)
+      else
+        nil
+      end
     end
 
     def action_paths_for(picture)
@@ -39,7 +43,7 @@ module Collectr
     def data_list_for(pictures)
       pictures.map do |picture|
         data_for(picture)
-      end
+      end.compact
     end
 
     def render_json_pictures(pictures)
